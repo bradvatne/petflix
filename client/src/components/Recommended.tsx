a2dc2dfadcf7b1233e84293ea7a2d56ab294d797
brad <brad.vatne@gmail.com>
2023-07-11 21:57:22 +0800

    trending query added

--- client/src/components/Recommended.tsx
+++ client/src/components/Recommended.tsx
@@ -2,8 +2,9 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store/reducer";
 
 export const Recommended = () => {
-  const movies = useSelector((state: RootState) => state.movies);
-  const featuredMovies = [];
+  const movies = useSelector((state: RootState) =>
+    state.movies.filter((movie) => movie.isTrending)
+  );
 
   return <div>{movies && movies.map((item) => <div>{item.title}</div>)}</div>;
 };
