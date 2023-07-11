d5a44a685e3bf5d6f9bb10cc3ae8909ccd1c550d
brad <brad.vatne@gmail.com>
2023-07-11 17:48:48 +0800

    navbar ui done

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { FC, ReactNode } from "react";
 import { Icons } from "./ui/Icons";
 
 export const NavBar = () => {
-  const links = [{}];
+  const links: NavItemType[] = [
+    {
+      id: 0,
+      title: "Featured",
+      icon: <Icons.Trending />,
+    },
+    {
+      id: 1,
+      title: "Movies",
+      icon: <Icons.Movies />,
+    },
+    {
+      id: 2,
+      title: "TV Shows",
+      icon: <Icons.TV />,
+    },
+    {
+      id: 3,
+      title: "Bookmarks",
+      icon: <Icons.BookMark />,
+    },
+  ];
 
   return (
-    <div className="m-8 w-24 bg-semidarkblue rounded-[1.25rem] flex flex-col items-center py-8">
-      <Icons.MovieLogo />
-      {links.map((link) => (
-        <NavItem navItem={link} />
-      ))}
+    <div className="m-8 w-24 max-h-[60rem] bg-semidarkblue rounded-[1.25rem] flex flex-col items-center justify-between py-8">
+      <div>
+        <Icons.MovieLogo />
+        <div className="flex flex-col gap-[40px] mt-[4.5rem] items-center">
+          {links.map((link) => (
+            <NavItem navItem={link} key={link.title} />
+          ))}
+        </div>
+      </div>
+      <div className="flex">
+        <Avatar />
+      </div>
     </div>
   );
 };
 
-type NavItemType = {};
+type NavItemType = {
+  id: number;
+  title: string;
+  icon: ReactNode;
+};
 
 const NavItem = ({ navItem }: { navItem: NavItemType }) => {
-  return <></>;
+  return <button>{navItem.icon}</button>;
+};
+
+const Avatar = () => {
+  return (
+    <div>
+      <img
+        src="/temp_portrait.png"
+        className="border border-white rounded-full"
+      />
+    </div>
+  );
 };
