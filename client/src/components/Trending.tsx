9ff3bc9edaff13d2d9076bae503d1d8f3ca322b0
brad <brad.vatne@gmail.com>
2023-07-12 10:31:26 +0800

    trendintg movies ui

--- client/src/components/Trending.tsx
+++ client/src/components/Trending.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store/reducer";
+import { Icons } from "./ui/Icons";
 
 export const Trending = () => {
   const movies = useSelector((state: RootState) =>
@@ -8,10 +9,10 @@ export const Trending = () => {
 
   return (
     <div className="overflow-hidden">
-      <h1 className="text-[2rem] font-[300] text-white tracking-[-0.03125rem]">
+      <h1 className="text-[2rem] font-[300] text-white tracking-[-0.03125rem] mb-[1.5rem]">
         Trending
       </h1>
-      <div className="flex overflow-x-hidden">
+      <div className="flex overflow-x-hidden gap-[2.5rem]">
         {movies &&
           movies.map((item, idx) => (
             <TrendingMovieCard movie={item} key={idx} />
@@ -24,7 +25,32 @@ export const Trending = () => {
 const TrendingMovieCard = ({ movie }: { movie: IMovie }) => {
   return (
     <div className="overflow-hidden flex shrink-0">
-      <div className="w-[470px] text-white">{movie.title}</div>
+      <div
+        className="w-[470px] h-[230px] text-white relative bg-contain rounded-lg overflow-hidden flex"
+        style={{ backgroundImage: `url(${movie.thumbnail.trending.large})` }}
+      >
+        <div className="flex items-end ml-6 mb-6">
+          <div>
+            <div className="flex gap-2 text-[0.9375rem] opacity-75 items-center w-full shrink-0">
+              {movie.year}
+              <Icons.Dot />
+              <div className="flex gap-[.3rem] items-center shrink-0 whitespace-nowrap">
+                <Icons.MovieSmall />
+                {movie.category}
+              </div>
+              <Icons.Dot />
+              {movie.rating}
+            </div>
+            <div className="text-[1.5rem] font-[500] whitespace-nowrap">{movie.title}</div>
+          </div>
+        </div>
+
+        <div className="flex w-full justify-end">
+          <div className="mt-4 mr-6 hover:cursor-pointer">
+            <Icons.BookMarkOutline />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
